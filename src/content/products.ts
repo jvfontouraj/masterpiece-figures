56e5670992dfd3cf0e005c6546b055280c27aa55
jvfontouraj <jvfontouraj@gmail.com>
2025-03-10 23:33:48 -0300

    imagens padronizadas produtos

--- src/content/products.ts
+++ src/content/products.ts
@@ -18,11 +18,12 @@ import SummerDrunkenBeauty from '@/assets/products/SUMMER DRUNKEN BEAUTY FLOWER 
 import Predator from '@/assets/products/predator_ahab.png'
 import PreadtorAmbientacao from '@/assets/products/ambientacoes/ambPredator Ahab.png'
 import PredatorAhab1 from '@/assets/products/predator-ahab/1.png'
-import PredatorAhab2 from '@/assets/products/predator-ahab/2.jpg'
-import PredatorAhab3 from '@/assets/products/predator-ahab/3.jpg'
+import PredatorAhab2 from '@/assets/products/predator-ahab/2.png'
+import PredatorAhab3 from '@/assets/products/predator-ahab/3.png'
 import PredatorAhab4 from '@/assets/products/predator-ahab/4.jpg'
 import PredatorAhab5 from '@/assets/products/predator-ahab/5.jpg'
 import PredatorAhab6 from '@/assets/products/predator-ahab/6.jpg'
+import PredatorAhab7 from '@/assets/products/predator-ahab/7.jpg'
 // //////////////////////////////
 
 // Alita ////////////////////////
@@ -34,6 +35,7 @@ import Alita3 from '@/assets/products/Alita/3.png'
 import Alita4 from '@/assets/products/Alita/4.png'
 import Alita5 from '@/assets/products/Alita/5.png'
 import Alita6 from '@/assets/products/Alita/6.png'
+import Alita7 from '@/assets/products/Alita/7.png'
 // //////////////////////////////
 
 // Wonder Woman /////////////////
@@ -144,11 +146,15 @@ export const products: ProductProps[] = [
         text: 'Criado pelos visionários cineastas James Cameron e Robert Rodriguez, o filme é baseado no mangá dos anos 90 Gunnm e na animação original Battle Angel. A protagonista, Alita, é uma ciborgue que não possui memórias de seu passado. Adotada pelo cientista Dr. Dyson Ido, ela se torna uma máquina de combate letal, mestre na arte marcial mecânica conhecida como PanzerKurst. Apesar de sua força, Alita precisa descobrir seu passado misterioso e usar suas habilidades únicas para salvar a si mesma, sua família e o mundo.',
         image: Alita1,
       },
+      section2: {
+        text: 'Além de sua impressionante presença visual, a estátua Alita Berserker Motorball Tryout é uma verdadeira obra-prima de engenharia e design. Cada detalhe foi meticulosamente esculpido para capturar a essência do personagem e a intensidade da cena. A base temática inspirada no estádio de Motorball, com seu efeito de iluminação LED, não só realça a dramaticidade da peça, mas também cria uma atmosfera envolvente que transporta os fãs diretamente para o universo cyberpunk do filme.',
+        video: Alita2,
+      },
       section3: {
-        image: Alita2,
+        image: Alita3,
       },
       section4: {
-        image: Alita3,
+        image: Alita4,
         text: [
           {
             text: 'Além de sua impressionante presença visual, a estátua Alita Berserker Motorball Tryout é uma verdadeira obraprima de engenharia e design. Cada detalhe foi meticulosamente esculpido para capturar a essência do personagem e a intensidade da cena. A base temática inspirada no estádio de Motorball, com seu efeito de iluminação LED, não só realça a dramaticidade da peça, mas também cria uma atmosfera envolvente que transporta os fãs diretamente para o universo cyberpunk do filme. <br/>A escolha do material, Polystone, garante durabilidade e um acabamento de alta qualidade, enquanto a escala ¼ proporciona uma presença imponente que se destaca em qualquer coleção34. Com dimensões impressionantes de 64,4 cm de altura, 76,4 cm de largura e 87,2 cm de diâmetro, e um peso total de 47 kg, esta estátua é uma peça central que certamente atrairá a atenção de todos.<br/><br/>Seja para exibição em uma coleção particular ou como destaque em um evento, a estátua Alita Berserker Motorball Tryout é uma adição indispensável para qualquer fã de Alita: Anjo de Combate.',
@@ -159,7 +165,7 @@ export const products: ProductProps[] = [
           },
         ],
       },
-      section5: [Alita4, Alita5, Alita6],
+      section5: [Alita5, Alita6, Alita7],
     },
   },
   {
@@ -363,10 +369,10 @@ export const products: ProductProps[] = [
       section2: {
         title: 'UM PREDADOR LENDÁRIO',
         text: 'Apelidado de "Ahab" por Galgo, este Predator foi inspirado no Capitão Ahab da obra de Herman Mel-ville, mas, em vez de uma baleia branca, Ahab está obcecado pela caça de sua presa suprema: um En-genheiro. Esta peça captura Ahab em um momento de sua vida após atingir esse objetivo, enquanto ele derrota uma centopeia gigante em sua caçada mais recente, apenas para "manter-se ocupado".',
-        video: PredatorAhab1,
+        video: PredatorAhab2,
       },
       section3: {
-        image: PredatorAhab2,
+        image: PredatorAhab3,
       },
       section4: {
         text: [
@@ -387,9 +393,9 @@ export const products: ProductProps[] = [
             text: 'Dimensões: A: 85 cm x L: 57 cm x P: 76 cm.</br>Peso Aproximado: 37 kg.</br>Materiais: Polystone e outros materiais de alta qualidade.',
           },
         ],
-        image: PredatorAhab3,
+        image: PredatorAhab4,
       },
-      section5: [PredatorAhab4, PredatorAhab5, PredatorAhab6],
+      section5: [PredatorAhab5, PredatorAhab6, PredatorAhab7],
     },
   },
   {
